8c3b4f01423325490c727db3c08f2f4e2238527a
Gray Manley <gray.manley@gmail.com>
2012-06-26 16:44:30 -0500

    Fix boot and allow for running "rails *" commands

--- core/lib/backstage/core/engine.rb
+++ core/lib/backstage/core/engine.rb
@@ -6,16 +6,16 @@ module Backstage
 
       config.autoload_paths += %W( #{config.root}/lib )
 
-      config.to_prepare &method(:include_backstage_core!).to_proc
+      # config.to_prepare &method(:include_backstage_core!).to_proc
 
       initializer "backstage.encoding" do |app|
         app.config.encoding = 'utf-8'
       end
 
-      def self.include_backstage_core!
-        ::ApplicationController.send(:include, Backstage::ApplicationController)
-        ::ApplicationController.send(:helper,  Backstage::Core::Engine.helpers)
-      end
+      # def self.include_backstage_core!
+      #   ::ApplicationController.send(:include, Backstage::ApplicationController)
+      #   ::ApplicationController.send(:helper,  Backstage::Core::Engine.helpers)
+      # end
     end
   end
 end
