ab9ff2f51ca384d07005f7235b49ca810daab6e1
tatums <tatum@ashlandstudios.com>
2013-03-13 20:56:52 -0500

    move attr_accessible to base

--- core/lib/backstage/core/product/base.rb
+++ core/lib/backstage/core/product/base.rb
@@ -1,9 +1,8 @@
 module Backstage::Core::Product::Base
   extend ActiveSupport::Concern
-
   include Pricing
 
   included do
-
+    attr_accessible :title, :description
   end
 end
--- storefronts/belly/config/routes.rb
+++ storefronts/belly/config/routes.rb
@@ -1,7 +1,4 @@
 Backstage::Core::Engine.register_routes(:belly) do
   root to: 'cool#index'
-
-  #match '/products' => 'products#index'
   resources :products, only: [:index, :show]
-
 end
