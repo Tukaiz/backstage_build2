ab5baef2e18aecc9b0514f233df8d77f3c6056ca
Abraham Oshel <abraham.oshel@gmail.com>
2013-03-14 02:28:56 -0500

    idea for current_store

--- core/app/controllers/application_controller.rb
+++ core/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   layout 'core/application'
   protect_from_forgery
+
+  def current_store
+    request.params[:storefront] || request.subdomain
+  end
 end
