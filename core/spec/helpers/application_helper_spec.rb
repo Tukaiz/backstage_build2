67feb0650ab73d3efb6abe13e7866e1b5bf2c067
tatums <tatum@ashlandstudios.com>
2013-03-29 17:01:25 -0500

    add application helper specs

---
+++ core/spec/helpers/application_helper_spec.rb
@@ -0,0 +1,30 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe Backstage::Core::Helpers::ApplicationHelper do
+
+  describe 'login_for' do
+
+    context 'When not Logged in' do
+      it 'display Login' do
+        expect( login_for nil ).to have_link('Login')
+      end
+
+      it 'has path of new_session' do
+        expect( login_for nil ).to have_selector('a[href="/sessions/new"]', text: 'Login')
+      end
+    end
+
+    context 'when Logged in' do
+      it 'should display Lgout' do
+        expect( login_for 'belly' ).to have_link('Logout')
+      end
+
+      it 'should display login link when not logged in' do
+        expect( login_for 'belly' ).to have_selector('a[href="/sessions/belly"]', text: 'Logout')
+      end
+
+    end
+
+  end
+
+end
