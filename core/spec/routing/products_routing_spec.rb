5a55295c174515ca33ca5538ede4714613c2bd44
tatums <tatum@ashlandstudios.com>
2013-03-31 22:48:02 -0500

    add routing spec - for future use

---
+++ core/spec/routing/products_routing_spec.rb
@@ -0,0 +1,16 @@
+#require File.expand_path('../../spec_helper', __FILE__)
+
+# describe "routes for products" do
+
+#   it "routes /products to the products controller" do
+#     expect(get "/products").to route_to(controller: 'products', action: 'index')
+#   end
+
+#   it "routes /products?storefront=belly to the belly::products controller" do
+#     pending do
+#       post new_session_path(:storefront => 'belly')
+#       expect(get "/products").to route_to(controller: 'belly/products', action: 'index')
+#     end
+#   end
+
+# end
